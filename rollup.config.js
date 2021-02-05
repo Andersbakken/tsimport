9efc5f6b420bd69b15fe18560358cdfb31029b50
Anders Bakken <agbakken@gmail.com>
2021-02-04 19:37:06 -0800

    Lose some stuff.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
-import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
-import jscc from "rollup-plugin-jscc";
-import replace from "@rollup/plugin-replace";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 
@@ -10,45 +7,18 @@ const input = "src/tsimport.ts";
 
 const plugins = [
     resolve({
-        preferBuiltins: false
+        preferBuiltins: true
     }),
     commonjs(),
-    replace({}),
-    jscc({
-        values: {}
-    }),
     typescript({
         tsconfig: `tsconfig.json`,
         cacheRoot: ".cache"
-    }),
-    babel({
-        exclude: "node_modules/**",
-        babelrc: false,
-        inputSourceMap: true,
-        sourceMaps: true,
-        babelHelpers: "bundled",
-        extensions: [".js", ".ts"],
-        presets: [
-            [
-                "@babel/preset-env",
-                {
-                    loose: true,
-                    targets: {
-                        safari: "6"
-                    },
-                    modules: false,
-                    useBuiltIns: "entry",
-                    corejs: 3,
-                    exclude: ["@babel/plugin-transform-async-to-generator", "@babel/plugin-transform-regenerator"]
-                }
-            ]
-        ]
     })
 ];
 
 // Define forms
 const format = "cjs";
-const external = [];
+const external = ["fs", "assert"];
 
 export default [
     {
