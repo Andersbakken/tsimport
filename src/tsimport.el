b43df1896ae52c4a10dd4a793f56c0084b4f3e73
Anders Bakken <agbakken@gmail.com>
2021-02-08 13:22:22 -0800

    stuff

--- src/tsimport.el
+++ src/tsimport.el
@@ -1,24 +1,26 @@
 (require 'thingatpt)
 
-(defvar ts-import-command "node /home/abakken/dev/tsimport/dist/tsimport.js")
-(defvar ts-import-backup-suffix nil)
-(defun ts-import (&optional symbol)
+(defvar tsimport-command "node /home/abakken/dev/tsimport/dist/tsimport.js")
+(defvar tsimport-backup-suffix nil)
+(defun tsimport (&optional symbol)
   (interactive)
   (unless symbol
     (let ((default (thing-at-point 'symbol))
-          (alternatives (split-string (shell-command-to-string (format "%s %s --complete" ts-import-command (buffer-file-name))))))
-      (setq symbol (completing-read (if default (format "Symbol (default %s): " default) "Symbol: ") alternatives))
+          (alternatives (split-string (shell-command-to-string (format "%s %s --complete" tsimport-command (buffer-file-name))))))
+      (when (and default (not (member default alternatives)))
+        (setq default nil))
+      (setq symbol (completing-read (if default (format "Symbol (default %s): " default) "Symbol: ") alternatives nil t nil nil default))
       (when (string= symbol "")
         (unless default
           (error "No default"))
         (setq symbol default))))
   (shell-command-to-string (format "%s %s %s -i%s"
-                                   ts-import-command
+                                   tsimport-command
                                    (buffer-file-name)
                                    symbol
-                                   (if ts-import-backup-suffix
-                                       (concat "=" ts-import-backup-suffix)
+                                   (if tsimport-backup-suffix
+                                       (concat "=" tsimport-backup-suffix)
                                      ""))))
   ;; (message "symbol %s" symbol))
 
-(provide 'ts-import)
+(provide 'tsimport)
