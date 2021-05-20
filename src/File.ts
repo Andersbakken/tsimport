440bef4029fa7e7e5d3c6f538d560df4ddfdad65
Anders Bakken <agbakken@gmail.com>
2021-05-20 10:56:13 -0700

    ws.

--- src/File.ts
+++ src/File.ts
@@ -6,6 +6,7 @@ export default class File {
     namedExports?: string[];
     defaultExport?: string;
     sourceCode: string;
+
     constructor(filePath: string, src: string) {
         this.path = filePath;
         this.sourceCode = src;
